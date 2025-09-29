7486dba1d1ce077d1f13c13f04e4c9820835853e
YashMagane <yash.magane.11@gmail.com>
2025-09-29 23:38:06 +0100

    completed task

---
+++ TodoList.ps1
@@ -0,0 +1,7 @@
+Todo list:
+add lines to nav -DONE
+constrict page width add drop shadow and background colour -DONE
+style home page table-DONE
+style services page table-DONE
+get rid of white background colour-DONE
+style contact form-DONE
